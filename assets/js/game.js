c679bacbb9e8837d362e67e12b7ce38fcd3283dd
souhila boukari <suhamimisou14@gmail.com>
2022-12-28 16:40:12 -0500

    merging to develop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@ function fight(){
     alert("The fight has begun!");
    var playerName = prompt("what is your robot name?");
    console.log(playerName);
-   console.log("this logs a string, good for leaving yourself a message");
+   
 console.log(10+10);
 console.log("our robot's name is " + playerName);
 }
