ff76aa6cab72ada211d908d88896ef238ce6a969
souhila boukari <suhamimisou14@gmail.com>
2023-01-13 16:11:31 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,18 @@
-// var playerName = 'Clank McKrank';
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames =["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
+
+
 // fight function
-var fight = function() {
+var fight = function(enemyNames) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -24,20 +24,20 @@ var fight = function() {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -65,7 +65,28 @@ var fight = function() {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+
 }; // end of fight function
 
-// run fight function to start game
+for(var i=0; i < enemyNames.length; i++ ){
+  fight(enemyNames[i]);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 fight();
+
